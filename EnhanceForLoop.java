6337808c301c40a9869de8ad49e63cf814980e5b
Minhajul43 <minhajul255685islam@gmail.com>
2026-03-26 09:17:54 +0600

    Basic java

---
+++ EnhanceForLoop.java
@@ -0,0 +1,10 @@
+// java enhance forEach loops
+import java.io.*;
+public class EnhanceForLoop {
+  public static void main(String[] args) {
+    String[] names = { "Minhajul", "Tahmeed", "Juel" };
+    for (String name : names) {
+      System.out.println("Name:" + name);
+    }
+  }
+}
---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+//java program demonstrates to working of for loops
+import java.io.*;
+
+public class ForLoop {
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.print(i + " ");
+    }
+  }
+}
